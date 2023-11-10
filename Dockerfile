1f7ed45a6d74ecff52de9d366e687cc961e4a8bd
Snikin013 <dan-013@yandex.ru>
2023-11-11 01:55:40 +0300

    1

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Используйте базовый образ Python
-FROM python:3.9-slim
+FROM python:3.8-slim
 
 # Установка переменной окружения для запуска внутри контейнера
 ENV PYTHONUNBUFFERED 1
--- app/models.py
+++ app/models.py
@@ -2,3 +2,7 @@
 
 # class Cabin(Base):
 #    __tablename__ = 'cabin'
+def model_predict(number_1, date, class_1, period):
+    predictions = (number for number in range(number_1))
+    print('predictions')
+    return predictions
